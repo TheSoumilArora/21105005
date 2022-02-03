2603d72d2448c8e6c502130627b1e5d90a75604b
Soumil Arora <soumil789@gmail.com>
2022-02-03 12:35:37 +0530

    Assignment 3

--- First year/Assignment 3/Q1.py
+++ First year/Assignment 3/Q1.py
@@ -1,2 +1,12 @@
 input_string = input("Enter the string : ").lower().split()
-print(input_string)
+if len(input_string) == 1:
+    input_string = input_string[0]
+occurences = {}
+for i in input_string:
+    if i in occurences:
+        occurences[i] += 1
+    else:
+        occurences[i] = 1
+print("The occurence(s) of :")
+for i in occurences:
+    print('\t"\033[1m%s\033[0m" is/are \033[1m%d\033[0m' % (i,occurences[i]))
