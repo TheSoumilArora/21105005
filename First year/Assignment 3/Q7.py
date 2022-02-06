b8abb0ee27b474ea34483f522586bfd2b1c3ec78
Soumil Arora <soumil789@gmail.com>
2022-02-06 14:27:22 +0530

    Assignment 3

---
+++ First year/Assignment 3/Q7.py
@@ -0,0 +1,14 @@
+def fibonacci(num):
+    a = 0
+    b = 1
+    if num < 0:
+        print("Incorrect input")
+    elif num == 0:
+        return 0
+    else:
+        for i in range(1, num):
+            c = a + b
+            a = b
+            b = c
+        return b
+print(fibonacci(int(input("Enter "))))
