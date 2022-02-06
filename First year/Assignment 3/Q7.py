742a9ccc4ca03798917daf5689c7b0622a64aa14
Soumil Arora <soumil789@gmail.com>
2022-02-06 20:32:15 +0530

    Assignment 3

--- First year/Assignment 3/Q7.py
+++ First year/Assignment 3/Q7.py
@@ -1,15 +1,23 @@
-def fibonacci(num):
-    a = 0
-    b = 1
+'''a is first number, b is second number and c is the extra variable used for adding and reassigning the values'''
+a = 0
+b = 1
+sum = 0
+while True:                                                                                                         #While loop is just for asking the user to input the value again if user enters a -ve value
+    num = int(input("Enter the no. of terms of the Fibonacci sequence: "))
     if num < 0:
-        print("Incorrect input")
-    elif num == 0:
-        return 0
+        print("Number of terms must be non-negative\nPlease enter again\n")
+        continue
+    if num == 0:
+        print("As the number of terms = 0, the average of all terms = 0")
+        break
     else:
-        for i in range(1, num):
+        print("The Fibonacci sequence is as follows:")
+        print(a,end=" ")
+        for i in range(1,num):
+            sum += b
+            print(b, end=" ")
             c = a + b
             a = b
             b = c
-        return b
-print(fibonacci(int(input("Enter "))))
-#Soumil Arora testing
+        print("\nThe average of the terms of Fibonacci sequence upto %d terms is %0.2f" % (num,sum/num))
+        break
--- First year/Assignment 3/Q8.py
+++ First year/Assignment 3/Q8.py
@@ -1,3 +1,26 @@
-set1 = [1,2,3,4,5]
-set2 = [2,4,6,8]
-set3 = [1,5,9,13,17]
+set1 = (1,2,3,4,5)
+set2 = (2,4,6,8)
+set3 = (1,5,9,13,17)
+print("a. Set of all elements that are in Set1 and Set2 but not both:",end=" ")
+
+set4 = list(set1 + set2)
+for i in set4:
+    if i in set1 and i in set2:
+        set4.remove(i)
+print(set4)
+
+print("b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3:",end=" ")
+
+pass
+
+print("c. Set of elements that are exactly two of the sets Set1, Set2 and Set3:",end=" ")
+
+pass
+
+print("d. Set of all integers in the range 1 to 10 that are not in Set1:",end=" ")
+
+pass
+
+print("e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3:",end=" ")
+
+pass
