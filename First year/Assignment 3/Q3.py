65ec272f0d39131cfe548d42d6772367f4e8e1f4
Soumil Arora <soumil789@gmail.com>
2022-02-14 12:51:34 +0530

    Assignment 3

--- First year/Assignment 3/Q3.py
+++ First year/Assignment 3/Q3.py
@@ -1,4 +1,4 @@
-list_in = eval(input("Enter the list: "))
+list_in = eval(input("Enter the list of integers: "))
 list_out = []
 for i in list_in:
     list_out.append((i, i**2))                                                                                      #(i, i**2) is the tuple which is added in the list, i.e. list_out is list of tuples
