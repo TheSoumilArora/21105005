327db37f988d9627441295a500c38cc12b3dd66a
Soumil Arora <soumil789@gmail.com>
2022-02-24 23:04:05 +0530

    Assignment 4

--- First year/Assignment 4/Q2.py
+++ First year/Assignment 4/Q2.py
@@ -4,16 +4,19 @@ n = int(input("Enter the number of rows you want to print: "))                  
 #RECURSION
 print("The Pascal's Triangle using recursion is:")
 def pascaltriangle(num):
-    lst_out = []
-    if num == 0:
-        print("topmost point = 1")
+    if num == 1:                                                                                    #Base case
+        return [[1]]
     else:
-        for i in range(num+1):
-            print(comb(num,i), end = " ")
-        print()
-        pascaltriangle(num-1)
-pascaltriangle(n)
-
+        result = pascaltriangle(num-1)                                                              #Recursive call
+        current_row = [1]                                                                           #The first element of each row, i.e. 1
+        previous_row = result[-1]                                                                   #Taking the last row from the data of all rows
+        for i in range(len(previous_row)-1):                                                        #Loop for adding the values of top 2 numbers for computing current number's value
+            current_row.append(previous_row[i] + previous_row[i+1])
+        current_row += [1]                                                                          #The last element of each row, i.e. 1
+        result.append(current_row)                                                                  #Adding the computed row to the data of all rows
+    return result
+for i in pascaltriangle(n):
+    print(i)
 
 #ITERATION
 print("The Pascal's Triangle using iteration is:")
