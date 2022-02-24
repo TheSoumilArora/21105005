7bb6973ef925888723470650e7d03955c0360dc8
Soumil Arora <soumil789@gmail.com>
2022-02-25 02:57:13 +0530

    Assignment 4

--- First year/Assignment 4/Q2.py
+++ First year/Assignment 4/Q2.py
@@ -1,31 +1,31 @@
 from math import comb
-n = int(input("Enter the number of rows you want to print: "))                                      #Asking the user the number of rows he/she wants
+n = int(input("Enter the number of rows you want to print: "))                                          #Asking the user the number of rows he/she wants
 
 #RECURSION
 print("The Pascal's Triangle using recursion is:")
 def pascaltriangle(num):
-    if num == 1:                                                                                    #Base case
+    if num == 1:                                                                                        #Base case
         return [[1]]
     else:
-        result = pascaltriangle(num-1)                                                              #Recursive call
-        current_row = [1]                                                                           #The first element of each row, i.e. 1
-        previous_row = result[-1]                                                                   #Taking the last row from the data of all rows
-        for i in range(len(previous_row)-1):                                                        #Loop for adding the values of top 2 numbers for computing current number's value
+        result = pascaltriangle(num-1)                                                                  #Recursive call
+        current_row = [1]                                                                               #The first element of each row, i.e. 1
+        previous_row = result[-1]                                                                       #Taking the last row from the data of all rows
+        for i in range(len(previous_row)-1):                                                            #Loop for adding the values of top 2 numbers for computing current number's value
             current_row.append(previous_row[i] + previous_row[i+1])
-        current_row += [1]                                                                          #The last element of each row, i.e. 1
-        result.append(current_row)                                                                  #Adding the computed row to the data of all rows
+        current_row += [1]                                                                              #The last element of each row, i.e. 1
+        result.append(current_row)                                                                      #Adding the computed row to the data of all rows
     return result
 for i in pascaltriangle(n):
-    print((n-len(i))*" ",end="")                                                                    #This print() adds space before printing each row
+    print((n-len(i))*" ",end="")                                                                        #This print() adds space before printing each row
     for j in i:
         print(j, end =" ")
     print()
 
 #ITERATION
 print("The Pascal's Triangle using iteration is:")
-for i in range(n):                                                                                  #Outer loop is for the rows
-    print((n-i-1)*" ",end="")                                                                       #This print() adds space before printing each row
-    for j in range(n):                                                                              #Inner loop is for the individual elements to be printed
-        if comb(i,j) != 0:                                                                          #Condition to ignore the cases when combination = 0 (when j>i)
-            print(comb(i,j),end=" ")                                                                #This print() prints each element and adds space after printing each value
-    print()                                                                                         #This print() is for changing line for next row
+for i in range(n):                                                                                      #Outer loop is for the rows
+    print((n-i-1)*" ",end="")                                                                           #This print() adds space before printing each row
+    for j in range(n):                                                                                  #Inner loop is for the individual elements to be printed
+        if comb(i,j) != 0:                                                                              #Condition to ignore the cases when combination = 0 (when j>i)
+            print(comb(i,j),end=" ")                                                                    #This print() prints each element and adds space after printing each value
+    print()                                                                                             #This print() is for changing line for next row
--- First year/Assignment 4/Q4.py
+++ First year/Assignment 4/Q4.py
@@ -1,9 +1,12 @@
-class Student:
+class Student:                                                                                          #Creating class
     def __init__(self,name,rno):
         self.name = name
         self.rno = rno
+        print("Object Created\n")
     def __del__(self):
         print("Object destroyed")
-
-
-        
+name = input("Enter name of student: ").strip()                                                         #Inputting name and roll number from the user
+roll_no = int(input("Enter SID of %s: " % (name)))
+student1 = Student(name,roll_no)                                                                        #Creating object
+print("The name of the student is %s and his/her roll number is %d" % (student1.name,student1.rno))     #Printing the assigned values
+del student1                                                                                            #Deleting the object
