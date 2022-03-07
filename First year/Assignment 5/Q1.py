833b2281c07b6a4595fb47d4306e44c9c26ee343
Soumil Arora <soumil789@gmail.com>
2022-03-08 01:18:24 +0530

    Assignment 5

--- First year/Assignment 5/Q1.py
+++ First year/Assignment 5/Q1.py
@@ -1,11 +1,29 @@
+#Importing required modules
 from tkinter import *
 
+
+#Defining all the required functions
 def add(x,y):
-    return x+y
+    return (x+y)
+
 def subtract(x,y):
-    return x-y
+    return (x-y)
+
 def multiply(x,y):
-    return x*y
+    return (x*y)
+
 def divide(x,y):
-    return x/y
-print(divide(3,4))
+    return (x/y)
+
+def gst(original_cost, net_price):
+    return ((net_price - original_cost) * 100 / original_cost)
+
+win = Tk()                                                                                              #Creating a GUI window
+win.title("GST Tax Finder Calculator")                                                                  #Setting the title
+win.geometry("300x300")
+win.minsize(height = "300", width = "300")
+win.maxsize(height = "1080", width = "1080")
+
+
+
+win.mainloop()
--- First year/Assignment 5/Q1_Testing2.py
+++ First year/Assignment 5/Q1_Testing2.py
@@ -3,7 +3,6 @@
 from tkinter import *
 from tkinter import messagebox
 win=Tk()
-win.maxsize(height='300',width='400')
 win.minsize(height='300',width='400')
 win.title('GST Tax Finder Calculator')
 Label_1=Label(win,text='GST Tax Finder Calculator',bg='red',font=('Algerian',20))
