5c19676cc944a960c30b55d4f0d8e60de0895b67
Soumil Arora <soumil789@gmail.com>
2022-03-08 12:16:05 +0530

    Assignment 5

--- First year/Assignment 5/Q1.py
+++ First year/Assignment 5/Q1.py
@@ -9,26 +9,27 @@ win.title("GST Tax Finder Calculator")                                          
 win.geometry("400x400")
 win.minsize(height = "400", width = "400")
 win.maxsize(height = "1080", width = "1080")
-win.configure(bg="Beige")
+win.configure(bg="Beige")                                                                               #Setting background colour
 
+#Heading
 heading = Label(win, text = "GST Tax Finder Calculator", borderwidth = 0, background = "Red", font = ("Montserrat ExtraBold",36))
 heading.pack(pady = 10,fill = X, ipady = 10)
 
-enter_original_cost = Label(win, text = "Enter the original cost", background = "Black")
-enter_original_cost.place(relheight = 20/400, relwidth = 140/400, relx=0.09, rely=0.24)                 #These values are experimental (hit-and-trial) to maintain the aspect ratio of the box when the window is resized
+#Original Cost entry
+enter_original_cost = Label(win, text = "Enter the original cost (in ₹)", background = "Black")
+enter_original_cost.place(relheight = 20/400, relwidth = 175/400, relx=0.06, rely=0.24)                 #These values are experimental (hit-and-trial) to maintain the aspect ratio of the box when the window is resized
 
 original_cost = IntVar(win, value = 0)
-original_cost_entry = Entry(win, justify="right", relief = "sunken", textvariable = original_cost, background = "Black")
-original_cost_entry.place(relheight = 20/400, relwidth = 140/400, relx = 0.545, rely=0.24)
+original_cost_entry = Entry(win, justify="right", relief = "sunken", textvariable = original_cost, background = "light blue",fg="black")
+original_cost_entry.place(relheight = 20/400, relwidth = 175/400, relx = 0.54, rely=0.24)
 
-enter_net_price = Label(win, text = "Enter the net price", background = "Black")
-enter_net_price.place(relheight = 20/400, relwidth = 140/400, relx=0.09, rely=0.36)                     #These values are experimental (hit-and-trial) to maintain the aspect ratio of the box when the window is resized
+#Net Price entry
+enter_net_price = Label(win, text = "Enter the net price (in ₹)", background = "Black")
+enter_net_price.place(relheight = 20/400, relwidth = 175/400, relx=0.06, rely=0.36)                     #These values are experimental (hit-and-trial) to maintain the aspect ratio of the box when the window is resized
 
 net_price = IntVar(win, value = 0)
-net_price_entry = Entry(win, justify="right", relief = "sunken", textvariable = original_cost, background = "Black")
-net_price_entry.place(relheight = 20/400, relwidth = 140/400, relx = 0.545, rely=0.36)
-
-
+net_price_entry = Entry(win, justify="right", relief = "sunken", textvariable = net_price, background = "light blue",fg="black")
+net_price_entry.place(relheight = 20/400, relwidth = 175/400, relx = 0.54, rely=0.36)
 
 
 
